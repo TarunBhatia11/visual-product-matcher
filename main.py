b4344560c7cb313e08715b3300ae9e46fc23f7c1
TARUN BHATIA <155810253+TarunBhatia11@users.noreply.github.com>
2025-08-31 17:06:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -30,6 +30,10 @@ def home():
 
 
 
+import os
+import uvicorn
+
 if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=False)
+    port = int(os.environ.get("PORT", 8000))  # 👈 Render gives PORT, else default 8000
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
+
